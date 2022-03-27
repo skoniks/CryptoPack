c36575468db54516a318e1937c9cde0b13745e8c
skoniks <skolya16@ya.ru>
2022-03-27 16:45:22 +0300

    Bitcoin builf update

--- bitcoin/entrypoint.sh
+++ bitcoin/entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
+echo "listen=0
+port=$PORT
+prune=$PRUNE
+server=1
+rpcuser=$RPC_USER
+rpcpassword=$RPC_PASS
+rpcbind=0.0.0.0
+rpcport=$RPC_PORT
+rpcallowip=0.0.0.0/0" \
+> /root/config.conf
+
+echo "BIN - $BIN"
+echo "RPC_PORT - $RPC_PORT"
+
 mkdir -p /root/wallets
-mkdir -p /var/bitcoin
-/usr/local/bin/bitcoind \
-  -conf=/root/bitcoin.conf \
-  -walletdir=/root/wallets \
-  -datadir=/var/bitcoin
+mkdir -p /var/blockchain
+/usr/local/bin/$BIN \
+-conf=/root/config.conf \
+-walletdir=/root/wallets \
+-datadir=/var/blockchain
