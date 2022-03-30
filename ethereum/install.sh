c42e9873625e1bfd46744ff3f96f49e1720e08c2
skoniks <skolya16@ya.ru>
2022-03-30 17:09:21 +0300

    Ethereum install update

--- ethereum/install.sh
+++ ethereum/install.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-apk add --no-cache git make go
+apk add --no-cache git make go linux-headers
 git clone https://github.com/ethereum/go-ethereum.git \
   --branch v1.10.16 --single-branch /tmp/ethereum
 cd /tmp/ethereum
+make geth -j $(nproc)
+cp ./build/bin/geth /usr/local/bin/
+apk del git make go linux-headers
+rm /tmp/ethereum/ -r
 
 
-apk del git make
+mkdir -p /var/blockchain
+geth --syncmode light --http --datadir /var/blockchain
+
+curl -X POST http://127.0.0.1:8545 \
+-H "Content-Type: application/json" \
+--data '{"jsonrpc":"2.0", "method":"eth_accounts","params":[], "id":1}'
